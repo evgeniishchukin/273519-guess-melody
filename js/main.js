aee6f74d747b18e58ca1016d767ddc4b61699178
evgeniyshchukin <evgeniy.shchukin@gmail.com>
2017-05-30 00:45:36 +0300

    Внес исправления

--- js/main.js
+++ js/main.js
@@ -1,65 +1,46 @@
-window.main = (function () {
-  const template = document.querySelector(`#templates`);                        // Находим template в DOM
-  const screensTemplate = template.content.querySelectorAll(`.main`);           // Находим экраны в template
-  const main = document.querySelector(`.main`);                                 // Находим элемент в DOM, в который будем вставлять экраны
-  const LEFT_ARROW = 37;                                                        // Определяем keyCode левой клавиши
-  const RIGHT_ARROW = 39;                                                       // Определяем keyCode правой клавиши
-  const screenOrder = [];                                                       // Создаем массим, в котором будут идти экраны по порядку
-  let newScreen;                                                                // Создаем элемент куда будем клонировать экраны
+(() => {
+  const template = document.querySelector(`#templates`);
+  const screensTemplates = template.content.querySelectorAll(`.main`);
+  const appBlock = document.querySelector(`.app`);
+  const LEFT_ARROW = 37;
+  const RIGHT_ARROW = 39;
+  const screensArray = [];
+  let screenIndex = null;
 
-  // Создаем функцию которая упакует экраны в массив в правильном порядке
-  const addOrederScreensInArray = (orderArray) => {
-    orderArray.forEach((item) => {
-      screenOrder.push(screensTemplate[item]);
-    });
+  const showScreen = (index) => {
+    appBlock.removeChild(appBlock.firstChild);
+    let newScreen = screensArray[index].cloneNode(true);
+    appBlock.insertBefore(newScreen, appBlock.firstChild);
+    screenIndex = index;
+    return screenIndex;
   };
 
-  // Создаем функция вставки экранов в DOM
-  const addScreen = (index) => {
-    if (main.querySelector(`.main`)) {
-      main.querySelector(`.main`).remove();
-    }
-    newScreen = screenOrder[index].cloneNode(true);
-    main.appendChild(newScreen);
-  };
+  [4, 3, 0, 1, 2].map((name) => {
+    screensArray.push(screensTemplates[name]);
+  });
 
-  // Запускаем ранее созданные функции
-  addOrederScreensInArray([4, 3, 0, 1, 2]);
-  addScreen(0);
+  showScreen(0);
 
-  // Навешиваем отслеживание нажатия клавиш на document
-  document.addEventListener(`keydown`, (event) => {
+  document.addEventListener(`keyup`, (event) => {
     if (event.altKey) {
-      if (event.keyCode === LEFT_ARROW) {
-        for (let i = 0; i < screenOrder.length; i++) {
-          if (`${main.querySelector(`.main`).classList}` === `${screenOrder[i].classList}`) {
-            i = i - 1;
-            if (i < 0) {
-              addScreen(4);
-              return;
-            } else {
-              addScreen(i);
-              return;
-            }
+      switch (event.keyCode) {
+        case (LEFT_ARROW):
+          if (screenIndex !== 0) {
+            screenIndex--;
+            showScreen(screenIndex);
           }
-        }
-      } else if (event.keyCode === RIGHT_ARROW) {
-        for (let i = 0; i < screenOrder.length; i++) {
-          if (`${main.querySelector(`.main`).classList}` === `${screenOrder[i].classList}`) {
-            i = i + 1;
-            if (i > 4) {
-              addScreen(0);
-              return;
-            } else {
-              addScreen(i);
+          break;
 
-              return;
-            }
+        case (RIGHT_ARROW):
+          if (screenIndex !== 4) {
+            screenIndex++;
+            showScreen(screenIndex);
           }
-        }
+          break;
+
+        default:
+          break;
       }
     }
   });
-
-
 })();
